964fca4ddb76c0854c206b171fbc3db779e92e22
CpE Lab BE 216 <CpE Lab BE 216@BE216-PC09>
2026-07-09 15:50:50 +0800

    updated cart

--- checkout.Designer.cs
+++ checkout.Designer.cs
@@ -43,6 +43,13 @@
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
+            materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
+            textBox8 = new TextBox();
+            materialLabel5 = new MaterialSkin.Controls.MaterialLabel();
+            textBox9 = new TextBox();
+            label5 = new Label();
+            textBox10 = new TextBox();
+            label6 = new Label();
             SuspendLayout();
             // 
             // materialLabel1
@@ -64,6 +71,8 @@
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(157, 30);
             textBox1.TabIndex = 1;
+            textBox1.Text = "First Name";
+            textBox1.TextChanged += this.textBox1_TextChanged;
             // 
             // textBox2
             // 
@@ -72,6 +81,7 @@
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(162, 30);
             textBox2.TabIndex = 2;
+            textBox2.Text = "Last Name";
             // 
             // textBox3
             // 
@@ -80,6 +90,7 @@
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(251, 30);
             textBox3.TabIndex = 3;
+            textBox3.Text = "@gmail.com";
             // 
             // materialLabel2
             // 
@@ -144,7 +155,7 @@
             materialButton1.Depth = 0;
             materialButton1.HighEmphasis = true;
             materialButton1.Icon = null;
-            materialButton1.Location = new Point(440, 601);
+            materialButton1.Location = new Point(412, 856);
             materialButton1.Margin = new Padding(4, 6, 4, 6);
             materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
             materialButton1.Name = "materialButton1";
@@ -192,11 +203,87 @@
             label4.TabIndex = 14;
             label4.Text = "POSTAL CODE";
             // 
+            // materialLabel4
+            // 
+            materialLabel4.AutoSize = true;
+            materialLabel4.Depth = 0;
+            materialLabel4.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialLabel4.Location = new Point(21, 603);
+            materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel4.Name = "materialLabel4";
+            materialLabel4.Size = new Size(159, 19);
+            materialLabel4.TabIndex = 15;
+            materialLabel4.Text = "CARD INFORMATION: ";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(21, 707);
+            textBox8.MinimumSize = new Size(0, 30);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(282, 30);
+            textBox8.TabIndex = 16;
+            textBox8.Text = "1234 1234 1234 1234";
+            // 
+            // materialLabel5
+            // 
+            materialLabel5.AutoSize = true;
+            materialLabel5.Depth = 0;
+            materialLabel5.Font = new Font("Roboto", 14F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialLabel5.Location = new Point(21, 676);
+            materialLabel5.MouseState = MaterialSkin.MouseState.HOVER;
+            materialLabel5.Name = "materialLabel5";
+            materialLabel5.Size = new Size(102, 19);
+            materialLabel5.TabIndex = 17;
+            materialLabel5.Text = "Card Number: ";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(21, 782);
+            textBox9.MinimumSize = new Size(0, 30);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(121, 30);
+            textBox9.TabIndex = 18;
+            textBox9.Text = "MM/YY";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(21, 815);
+            label5.Name = "label5";
+            label5.Size = new Size(59, 15);
+            label5.TabIndex = 19;
+            label5.Text = "Expiration";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(182, 782);
+            textBox10.MinimumSize = new Size(0, 30);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(121, 30);
+            textBox10.TabIndex = 20;
+            textBox10.Text = "CVC";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(182, 815);
+            label6.Name = "label6";
+            label6.Size = new Size(30, 15);
+            label6.TabIndex = 21;
+            label6.Text = "CVC";
+            // 
             // checkout
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1519, 687);
+            ClientSize = new Size(1519, 931);
+            Controls.Add(label6);
+            Controls.Add(textBox10);
+            Controls.Add(label5);
+            Controls.Add(textBox9);
+            Controls.Add(materialLabel5);
+            Controls.Add(textBox8);
+            Controls.Add(materialLabel4);
             Controls.Add(label4);
             Controls.Add(label3);
             Controls.Add(label2);
@@ -219,6 +306,11 @@
             PerformLayout();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         #endregion
 
         private MaterialSkin.Controls.MaterialLabel materialLabel1;
@@ -236,5 +328,12 @@
         private Label label2;
         private Label label3;
         private Label label4;
+        private MaterialSkin.Controls.MaterialLabel materialLabel4;
+        private TextBox textBox8;
+        private MaterialSkin.Controls.MaterialLabel materialLabel5;
+        private TextBox textBox9;
+        private Label label5;
+        private TextBox textBox10;
+        private Label label6;
     }
 }
